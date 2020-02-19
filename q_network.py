9bfb81325fe9f2de8f079f47aec7fce1881f2e71
Cliff Li <cliff.yuan.li@gmail.com>
2020-02-19 16:19:56 +0100

    add replay buffer

--- q_network.py
+++ q_network.py
@@ -8,8 +8,10 @@ import tensorflow as tf
 class QNetwork(object):
     def __init__(self):
         # Params
-        self.n_train_episodes = 5000
+        self.n_train_episodes = 20000
+        self.n_train_iterations = 20
         self.n_batch_episodes = 100
+        self.n_epochs = 20
         self.epsilon = 0.1
         self.gamma = 1.0
 
@@ -22,9 +24,11 @@ class QNetwork(object):
 
     def _init_model(self):
         self._model = tf.keras.Sequential([
-            tf.keras.layers.Dense(64, activation='relu',
+            tf.keras.layers.Dense(10, activation='relu',
                                   input_shape=(104,), dtype=tf.dtypes.float16),
-            tf.keras.layers.Dense(64, activation='relu',
+            tf.keras.layers.Dense(10, activation='relu',
+                                  dtype=tf.dtypes.float16),
+            tf.keras.layers.Dense(3, activation='relu',
                                   dtype=tf.dtypes.float16),
             tf.keras.layers.Dense(2, dtype=tf.dtypes.float16)
         ])
@@ -49,7 +53,7 @@ class QNetwork(object):
         return self.transform_state(state), reward, terminal
 
     def train(self):
-        experiences = []
+        replay_buffer = []
         for t in range(self.n_train_episodes):
             state = self.transform_state(self.env.reset())
             terminal = False
@@ -57,33 +61,39 @@ class QNetwork(object):
             while not terminal:
                 action = self.get_action(state)
                 state_next, reward, terminal = self.act_and_observe(action)
-                experiences.append(
+                replay_buffer.append(
                     (state, action, reward, state_next, terminal))
                 state = state_next
 
-            if t % self.n_batch_episodes == 0:
-                states = []
-                actions = []
-                rewards = []
-                states_next = []
-                terminals = []
-                for experience in experiences:
-                    states.append(experience[0])
-                    actions.append(experience[1])
-                    rewards.append(experience[2])
-                    states_next.append(experience[3])
-                    terminals.append(experience[4])
-                states = np.array(states)
-                states_next = np.array(states_next)
-                q_next = self._model.predict(states_next)
-                q_next = np.max(q_next, axis=1)
-                q_expected = np.array(rewards) + \
-                    (1 - np.array(terminals)) * self.gamma * q_next
-                q = self._model.predict(states)
-                for i, action in enumerate(actions):
-                    q[i][action] = q_expected[i]
-                print(self._model.train_on_batch(states, q))
-                experiences = []
+            if t % self.n_batch_episodes == 0 and len(replay_buffer) > 100:
+                loss_sum = 0
+                for _ in range(self.n_train_iterations):
+                    states = []
+                    actions = []
+                    rewards = []
+                    states_next = []
+                    terminals = []
+                    for idx in np.random.randint(0, len(replay_buffer), 100):
+                        experience = replay_buffer[idx]
+                        states.append(experience[0])
+                        actions.append(experience[1])
+                        rewards.append(experience[2])
+                        states_next.append(experience[3])
+                        terminals.append(experience[4])
+                    states = np.array(states)
+                    states_next = np.array(states_next)
+                    q_next = self._model.predict(states_next)
+                    q_next = np.max(q_next, axis=1)
+                    q_expected = np.array(rewards) + \
+                        (1 - np.array(terminals)) * self.gamma * q_next
+                    q = self._model.predict(states)
+                    for i, action in enumerate(actions):
+                        q[i][action] = q_expected[i]
+                    history = self._model.fit(
+                        states, q, epochs=self.n_epochs, verbose=0)
+                    loss_sum += history.history['loss'][-1]
+                print('{}/{}: {}'.format(t, self.n_train_episodes,
+                                         loss_sum / self.n_train_iterations))
         print('Finished training ...')
 
     def play(self):
@@ -97,7 +107,8 @@ class QNetwork(object):
                 action = self.get_action(state, explore=False)
                 state, reward, terminal = self.act_and_observe(action)
                 cummulative_reward += reward
-        print('Avg. reward: {}'.format(cummulative_reward / self.n_play_episodes))
+        print('\nAvg. reward: {}'.format(
+            cummulative_reward / self.n_play_episodes))
 
 
 def main():
